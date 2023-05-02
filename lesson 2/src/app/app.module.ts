36b6ee0348e6d5f84b942acba4c38bb9017ccd2e
Roman-Kachmarek <kachmarek.rk@gmail.com>
2023-05-03 02:53:47 +0300

    Angular user list

---
+++ lesson 2/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/Forms';
+
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Lesson02Component } from './lesson02/lesson02.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Lesson02Component,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ lesson 2/src/app/lesson02/lesson02.component.ts
@@ -0,0 +1,83 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lesson02',
+  templateUrl: './lesson02.component.html',
+  styleUrls: ['./lesson02.component.scss'],
+})
+export class Lesson02Component {
+  public placeholderLogin = 'Enter login';
+  public placeholderPass = 'Enter password';
+  public placeholderMail = 'Enter email';
+  public loginMain = 'login-main';
+  public passMain = 'pass-main';
+  public mailMain = 'mail-main';
+  public heading = 'header';
+  public loginInput = 'login-input';
+  public passInput = 'pass-input';
+  public mailInput = 'mail-input';
+  public btnMain = 'btn-main';
+  public userBtn = 'user-button';
+  public tableHeader = 'table-header';
+  public editBtn = 'btn-edit';
+  public deleteBtn = 'btn-delete';
+  public updateBtn = 'edit-btn';
+  public editStatus = false;
+  public editIndex!: number;
+
+  public user = {
+    login: '',
+    pass: '',
+    email: '',
+  };
+
+  public userArr: Array<{
+    login: string;
+    pass: string;
+    email: string;
+  }> = [];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  addUser(): void {
+    this.userArr.push(this.render());
+    this.clearField();
+  }
+
+  deleteUser(index: number): void {
+    console.log('user deleted');
+    this.userArr.splice(index, 1);
+  }
+
+  editUser(index: number): void {
+    this.user.login = this.userArr[index].login;
+    this.user.pass = this.userArr[index].pass;
+    this.user.email = this.userArr[index].email;
+    this.editIndex = index;
+    this.editStatus = true;
+  }
+
+  saveEditUser(): void {
+    this.editStatus = false;
+    this.userArr[this.editIndex].login = this.user.login;
+    this.userArr[this.editIndex].pass = this.user.pass;
+    this.userArr[this.editIndex].email = this.user.email;
+    this.clearField();
+  }
+
+  render() {
+    return {
+      login: this.user.login,
+      pass: this.user.pass,
+      email: this.user.email,
+    };
+  }
+
+  clearField(): void {
+    this.user.login = '';
+    this.user.email = '';
+    this.user.pass = '';
+  }
+}
